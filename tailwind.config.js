01a43f2f9eeaf96f99f8380faa5afd246071eab1
Takayuki Miura <takayuki.miura28@gmail.com>
2024-05-12 20:00:55 +0900

    feat: 色の設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff8ff",
+          100: "#ddf1ff",
+          200: "#b4e4ff",
+          300: "#71cfff",
+          400: "#27b7ff",
+          500: "#009efd",
+          600: "#007dd9",
+          700: "#0063af",
+          800: "#005390",
+          900: "#034577",
+          950: "#011526",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
