bb79a3c089546fe9dae361b7356c7b9068783623
Takayuki Miura <takayuki.miura28@gmail.com>
2024-08-23 19:05:49 +0900

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+import { getBlogPostList } from "./blog/utils";
+
+const endpoint = "https://www.kappy.dev";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getBlogPostList();
+  return [
+    {
+      url: endpoint,
+    },
+    {
+      url: `${endpoint}/blog`,
+    },
+    ...posts.map((post) => ({
+      url: `${endpoint}/blog/${post.slug}`,
+    })),
+  ];
+}
